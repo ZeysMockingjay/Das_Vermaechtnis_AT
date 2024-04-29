1fcac0d41105356600efe213c02a56f7d4b635cd
Zeys <zabmaxgaming@gmail.com>
2024-04-29 17:15:19 +0200

    Level Change is now working.

---
+++ Area2DMap1.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var entered = false
+
+func _on_Area2D_body_entered(body: CharacterBody2D) -> void:
+	entered = true
+
+func _on_Area2D_body_exited(body: CharacterBody2D) -> void:
+	entered = false
+
+func _ready():
+	# Correctly connect signals using Callable
+	connect("body_entered", Callable(self, "_on_Area2D_body_entered"))
+	connect("body_exited", Callable(self, "_on_Area2D_body_exited"))
+
+func _physics_process(_delta: float) -> void:
+	if entered:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://Test_level_levelchangetest.tscn")
