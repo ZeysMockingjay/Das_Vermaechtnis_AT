912a4358d4ddc65d7e20add11cd2eed3a48974f4
Zeys <zabmaxgaming@gmail.com>
2024-04-29 14:47:56 +0200

    Project Repository gestartet
    yuhh

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const GRAVITY : int = 200
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta  # Continuously apply gravity to simulate falling or staying on the ground
+
+	# Reset horizontal velocity before checking input
+	velocity.x = 0
+
+	# Handle horizontal movement input
+	if Input.is_action_pressed("playermoveright"):
+		velocity.x = 100  # Set horizontal speed for moving right
+	elif Input.is_action_pressed("playermoveleft"):
+		velocity.x = -100  # Set horizontal speed for moving left
+
+	# Update animation based on current velocity state
+	if is_on_floor():
+		if velocity.x != 0:
+			$AnimatedSprite2D.play("run")  # Play running animation if moving horizontally
+		else:
+			$AnimatedSprite2D.play("idle")  # Play idle animation if not moving horizontally
+
+	# Execute the movement
+	move_and_slide()
---
+++ test_level.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# Game variables
+const PLAYER_START_POS : Vector2 = Vector2(17, 106)
+#TESTconst CAM_START_POS : Vector2 = Vector2(136, 77)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+# Initialize a new game, setting initial positions and properties
+func new_game():
+	$Player.position = PLAYER_START_POS
+	$Player.velocity = Vector2.ZERO  # Ensure the player's velocity is reset
+	#TEST$Camera2D.global_position = CAM_START_POS  # Optionally set an initial camera position
+	$Boden.position = Vector2(500, 191)  # Positioning the ground if needed
+
+# No longer modifying the player's or camera's position in _process
+
+
+# (For scrolling ground in the train level?)
+# if $Camera2D.position.x - $Boden.position.x > get_window().size.x * 1.5:
+#    $Boden.position.x += get_window().size.x  # Reset ground position
