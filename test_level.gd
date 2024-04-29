7bc12aa66a8dc8548c177bf0c4a424bbaad8073a
Zeys <zabmaxgaming@gmail.com>
2024-04-29 15:02:47 +0200

    Code Test

--- test_level.gd
+++ test_level.gd
@@ -17,7 +17,7 @@ func new_game():
 
 # No longer modifying the player's or camera's position in _process
 
-#Testyo Walter White yo yo
+#Testyo Walter White yo yo nochmal yo
 
 # (For scrolling ground in the train level?)
 # if $Camera2D.position.x - $Boden.position.x > get_window().size.x * 1.5:
