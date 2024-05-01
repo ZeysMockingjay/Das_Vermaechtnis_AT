126bbb335aeba04b59341db50f7f471b604d9d80
Zeys <zabmaxgaming@gmail.com>
2024-05-01 03:43:46 +0200

    camera and border fixes

--- Player.gd
+++ Player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-const GRAVITY : int = 500 #OG 200
+const GRAVITY : int = 500
 
-func _physics_process(delta):
-	velocity.y += GRAVITY * delta  # Continuously apply gravity to simulate falling or staying on the ground
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta  # Apply gravity
 
 	# Reset horizontal velocity before checking input
 	velocity.x = 0
@@ -14,13 +14,12 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("playermoveleft"):
 		velocity.x = -100  # Set horizontal speed for moving left
 
-	# Update animation based on current velocity state
-	if is_on_floor():
-		if velocity.x == 0:
-			$AnimatedSprite2D.play("idle")
-		if velocity.x != 0:
-			$AnimatedSprite2D.play("run")  # Play running animation if moving horizontally
-		
-
-	# Execute the movement
+	# Move and slide, no arguments needed
 	move_and_slide()
+
+	# Check if the player is on the floor and update animations based on actual movement
+	if is_on_floor():
+		if velocity.x == 0 or (velocity.x != 0 and is_on_wall()):
+			$AnimatedSprite2D.play("idle")  # Play idle animation if there's no effective horizontal movement or if stuck on a wall
+		else:
+			$AnimatedSprite2D.play("run")  # Play running animation if moving horizontally
