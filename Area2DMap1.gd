e49044cb28ce08fb5d37a28236f39e9d43e99cb9
Zeys <zabmaxgaming@gmail.com>
2024-05-03 03:52:34 +0200

    fixes for transition

--- Area2DMap1.gd
+++ Area2DMap1.gd
@@ -31,7 +31,7 @@ func _physics_process(_delta: float) -> void:
 	if entered:
 		if Input.is_action_just_pressed("ui_accept"):
 			TransitionScreen.transition()
-			#await TransitionScreen.on_transition_finished()
+			await TransitionScreen.on_transition_finished()
 			get_tree().change_scene_to_file("res://Test_level_levelchangetest.tscn")
 			
 
