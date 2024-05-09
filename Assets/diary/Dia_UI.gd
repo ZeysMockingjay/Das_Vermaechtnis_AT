cee415a2844f53454a2c76a10ebe963aaa04fd43
Zeys <zabmaxgaming@gmail.com>
2024-05-10 00:04:38 +0200

    Diary script changes
    removed smoothing script, changed Diary script

--- Assets/diary/Dia_UI.gd
+++ Assets/diary/Dia_UI.gd
@@ -1,10 +1,19 @@
 extends Control
 
+@onready var dia: Dia = preload("res://Assets/diary/playerdia.tres")
+@onready var slots: Array = $TextureRect/GridContainer.get_children()
+
+
 var is_open = false
 
 func _ready():
+	update_slots()
 	close()
 	
+func update_slots():
+	for i in range(min(dia.clues.size(), slots.size())):
+		slots[i].update(dia.clues[i])
+
 func _process(delta):
 	if Input.is_action_just_pressed("Space"):
 		if is_open:
