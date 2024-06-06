9da5fcb1437f379a0ed25474bc69be715ccaf546
Zeys <zabmaxgaming@gmail.com>
2024-06-07 01:17:06 +0200

    more experiments with cutscenes

---
+++ Node2D.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("Bike")
+
+func play_anim(Cutscene1_Intro_Anim) -> void:
+	anim.play(Cutscene1_Intro_Anim)
+
+func stop_anim() -> void:
+	anim.stop()
+
+#NOTIZEN: 
+# Animation Name entweder Cutscene1_Intro_Anim oder Bike
+#
+#
+#
+#
+#
--- test_level.gd
+++ test_level.gd
@@ -1,7 +1,4 @@
-#SKRIPT VON WELT 1 NODE
-
 extends Node2D
-
 
 var entered = false
 
@@ -12,16 +9,40 @@ func _on_Area2D_body_exited(body: CharacterBody2D) -> void:
 	entered = false
 
 func _ready():
-	
+	# Load the scene containing the AnimationPlayer
+	var cutscene_scene = load("res://Beginning_Animation.tscn")
+	if cutscene_scene:
+		var cutscene_instance = cutscene_scene.instantiate()
+		add_child(cutscene_instance)
+		
+		# Get the AnimationPlayer node from the instantiated scene
+		var animation_player = cutscene_instance.get_node("AnimationPlayer")
+		
+		# Debug: print the node to verify it exists
+		print(animation_player)
+		if animation_player:
+			# Play the animation
+			animation_player.play("Cutscene1_Intro_Anim")
+			# Connect the animation_finished signal to a function
+			animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))
+			# Initially disable gameplay elements
+			set_physics_process(false)
+		else:
+			print("AnimationPlayer node not found!")
+	else:
+		print("Failed to load cutscene scene!")
+
 	connect("body_entered", Callable(self, "_on_Area2D_body_entered"))
 	connect("body_exited", Callable(self, "_on_Area2D_body_exited"))
-	
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "Cutscene1_Intro_Anim":
+		# Enable gameplay elements after the animation finishes
+		set_physics_process(true)
 
 func _physics_process(_delta: float) -> void:
 	if entered:
 		if Input.is_action_just_pressed("ui_accept"):
 			TransitionScreen.transition()
-			#await TransitionScreen.on_transition_finished()
+			# await TransitionScreen.on_transition_finished()
 			get_tree().change_scene_to_file("res://Test_level_levelchangetest.tscn")
-			
-
