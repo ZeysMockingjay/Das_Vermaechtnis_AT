dd2ad97e1ae0ce97a2e48adbed3b2e0cde67c1e6
Zeys <zabmaxgaming@gmail.com>
2024-06-07 01:21:17 +0200

    misc

--- Node2D.gd
+++ Node2D.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var anim: AnimationPlayer = $AnimationPlayerNode/AnimationPlayer #HIER VERVOLLSTÄNDIGE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
