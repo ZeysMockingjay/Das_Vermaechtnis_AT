ef4cf4aa11d09170eaffee15fc321a5f52a5cf36
Zeys <zabmaxgaming@gmail.com>
2024-07-10 21:43:26 +0200

    misc
    -fixed door z-index

--- Bücherregalskript.gd
+++ Bücherregalskript.gd
@@ -1,11 +1,34 @@
 extends CollisionShape2D
 
+var entered = false
+var bib_regalview_scene = preload("res://Bib_Regalview.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready() -> void:
+	var parent_area = get_parent()
+	if parent_area is Area2D:
+		parent_area.connect("body_entered", Callable(self, "_on_Area2D_body_entered"))
+		parent_area.connect("body_exited", Callable(self, "_on_Area2D_body_exited"))
+	else:
+		push_error("Parent node is not an Area2D. This script should be attached to a CollisionShape2D that is a child of an Area2D.")
 
+func _on_Area2D_body_entered(body: Node) -> void:
+	if body is CharacterBody2D:
+		entered = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_Area2D_body_exited(body: Node) -> void:
+	if body is CharacterBody2D:
+		entered = false
+
+func _physics_process(_delta: float) -> void:
+	if entered and Input.is_action_just_pressed("e"):
+		open_bib_regalview()
+
+func open_bib_regalview() -> void:
+	TransitionScreen.transition()
+	await TransitionScreen.on_animation_finished
+	
+	var bib_regalview_instance = bib_regalview_scene.instantiate()
+	
+	var canvas_layer = CanvasLayer.new()
+	get_tree().root.add_child(canvas_layer)
+	canvas_layer.add_child(bib_regalview_instance)
